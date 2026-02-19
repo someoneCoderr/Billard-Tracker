e704da2117ccb8d484ea09b2e8c9558ca0c4bad9
Silas Haelg <silas.haelg@bluewin.ch>
2026-02-19 16:51:51 +0100

    First Database connection

---
+++ Billard-Tracker/Data/Database.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace Billard_Tracker.Data
+{
+    public class Database
+    {
+        public Database()
+        {
+            var baseDir = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "BillardTracker");
+
+            Directory.CreateDirectory(baseDir);
+
+            var dbPath = Path.Combine(baseDir, "billard_tracker.db");
+            var connectionString = $"Data Source={dbPath};";
+
+            try
+            {
+                using var connection = new SqliteConnection(connectionString);
+                connection.Open();
+                MessageBox.Show("Connection good");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection failed: {ex.Message}");
+                MessageBox.Show(dbPath);
+            }
+        }
+    }
+}
--- Billard-Tracker/MainWindow.xaml.cs
+++ Billard-Tracker/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Billard_Tracker
 {
@@ -18,6 +9,7 @@ namespace Billard_Tracker
     {
         public MainWindow()
         {
+            var db = new Data.Database();
             InitializeComponent();
         }
     }
